5ecd551f7f508865c0ea76db5952977ecc9ba64d
ashish sonam <ashish.sonam@setu.co>
2025-03-21 11:31:48 +0530

    change start.sh

--- start.sh
+++ start.sh
@@ -5,20 +5,11 @@ PGB_DIR="/home/pgbouncer"
 INI="${PGB_DIR}/pgbouncer.ini"
 USERLIST="${PGB_DIR}/userlist.txt"
 
+rm -f "${INI}" "${USERLIST}"
+
+PGB_DATABASES="host=organisatie-dev-eks-qa-db-2.cgaimxq41enr.ap-south-1.rds.amazonaws.com port=5432 user=setudbuser password=bmL829U6PvHmff72KGIg6BZDIi4183Ac dbname=setudb topology_query='select endpoint from rds_tools.show_topology(\'pgbouncer\')'"
 PGB_ADMIN_USERS="setudbuser"
 PGB_ADMIN_PASSWORDS="bmL829U6PvHmff72KGIg6BZDIi4183Ac"
-
-# if [ -z ${PGB_ADMIN_USERS+x} ]; then
-#   PGB_ADMIN_USERS="setudbuser"
-#   PGB_ADMIN_PASSWORDS="bmL829U6PvHmff72KGIg6BZDIi4183Ac"
-# fi
-
-# Auto-generate conf if it doesn't exist
-# if [ ! -f ${INI} ]; then
-#   if [[ -z "${PGB_DATABASES:-}" ]]; then
-#     echo "Error: no databases specified in \$PGB_DATABASES"
-#     exit 1
-#   fi
 
 cat <<- END > $INI
 [databases]
@@ -34,9 +25,9 @@ cat <<- END > $INI
     log_pooler_errors = 1
     routing_rules_py_module_file = /home/pgbouncer/routing_rules.py
     log_stats = 1
-    auth_file = /home/pgbouncer/userlist.txt
-    logfile = /home/pgbouncer/pgbouncer.log
-    pidfile = /home/pgbouncer/pgbouncer.pid
+    auth_file = $USERLIST
+    logfile = $PGB_DIR/pgbouncer.log
+    pidfile = $PGB_DIR/pgbouncer.pid
     admin_users = admin
     pool_mode = transaction
     max_client_conn = 10000
@@ -44,25 +35,22 @@ cat <<- END > $INI
     server_failed_delay = 5
     recreate_disconnected_pools = 1
 END
-  cat $INI
+
+cat $INI
+
+# convert comma-separated string variables to arrays.
+IFS=',' read -ra admin_array <<< "$PGB_ADMIN_USERS"
+IFS=',' read -ra password_array <<< "$PGB_ADMIN_PASSWORDS"
+
+# check every admin account has a corresponding password, and vice versa
+if (( ${#admin_array[@]} != ${#password_array[@]} )); then
+    exit 1
 fi
 
-# Auto-generate conf if it doesn't exist
-if [ ! -f ${USERLIST} ]; then
-  # convert comma-separated string variables to arrays.
-  IFS=',' read -ra admin_array <<< "$PGB_ADMIN_USERS"
-  IFS=',' read -ra password_array <<< "$PGB_ADMIN_PASSWORDS"
-
-  # check every admin account has a corresponding password, and vice versa
-  if (( ${#admin_array[@]} != ${#password_array[@]} )); then
-      exit 1
-  fi
-
-  # Zip admin arrays together and write them to userlist.
-  for (( i=0; i < ${#admin_array[*]}; ++i )); do
-      echo "\"${admin_array[$i]}\" \"${password_array[$i]}\"" >> $USERLIST
-  done
-fi
+# Zip admin arrays together and write them to userlist.
+for (( i=0; i < ${#admin_array[*]}; ++i )); do
+    echo "\"${admin_array[$i]}\" \"${password_array[$i]}\"" >> $USERLIST
+done
 
 chmod 0600 $INI
 chmod 0600 $USERLIST
