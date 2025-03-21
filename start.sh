fa040b692396916127186eba80ea9e1c4280cfbe
ashish sonam <ashish.sonam@setu.co>
2025-03-21 13:54:22 +0530

    changes

--- start.sh
+++ start.sh
@@ -5,55 +5,49 @@ PGB_DIR="/home/pgbouncer"
 INI="${PGB_DIR}/pgbouncer.ini"
 USERLIST="${PGB_DIR}/userlist.txt"
 
-if [ -z ${PGB_ADMIN_USERS+x} ]; then
-  PGB_ADMIN_USERS="admin"
-  PGB_ADMIN_PASSWORDS="pw"
-fi
-
-# Auto-generate conf if it doesn't exist
-if [ ! -f ${INI} ]; then
-  if [[ -z "${PGB_DATABASES:-}" ]]; then
-    echo "Error: no databases specified in \$PGB_DATABASES"
-    exit 1
-  fi
+PGB_ADMIN_USERS="setudbuser"
+PGB_ADMIN_PASSWORDS="bmL829U6PvHmff72KGIg6BZDIi4183Ac"
 
 cat <<- END > $INI
 [databases]
-    * = $PGB_DATABASES
+    setudb = host=organisatie-dev-eks-qa-db-2.cgaimxq41enr.ap-south-1.rds.amazonaws.com port=5432 user=setudbuser password=bmL829U6PvHmff72KGIg6BZDIi4183Ac dbname=setudb topology_query='select endpoint from rds_tools.show_topology(\'pgbouncer\')'
 [pgbouncer]
-    listen_port = ${PGB_LISTEN_PORT:-5432}
-    listen_addr = ${PGB_LISTEN_ADDR:-0.0.0.0}
+    listen_port = 6432
+    listen_addr = *
     auth_type = md5
+    server_tls_sslmode = require
     default_pool_size = 20
     log_connections = 1
     log_disconnections = 1
     log_pooler_errors = 1
     routing_rules_py_module_file = /home/pgbouncer/routing_rules.py
     log_stats = 1
-    auth_file = $USERLIST
-    logfile = $PGB_DIR/pgbouncer.log
-    pidfile = $PGB_DIR/pgbouncer.pid
-    admin_users = ${PGB_ADMIN_USERS:-admin}
+    auth_file = /home/pgbouncer/userlist.txt
+    logfile = /home/pgbouncer/pgbouncer.log
+    pidfile = /home/pgbouncer/pgbouncer.pid
+    admin_users = admin
+    pool_mode = transaction
+    max_client_conn = 10000
+    polling_frequency = 100
+    server_failed_delay = 5
+    recreate_disconnected_pools = 1
 END
-  cat $INI
+  
+cat $INI
+
+# convert comma-separated string variables to arrays.
+IFS=',' read -ra admin_array <<< "$PGB_ADMIN_USERS"
+IFS=',' read -ra password_array <<< "$PGB_ADMIN_PASSWORDS"
+
+# check every admin account has a corresponding password, and vice versa
+if (( ${#admin_array[@]} != ${#password_array[@]} )); then
+    exit 1
 fi
 
-# Auto-generate conf if it doesn't exist
-if [ ! -f ${USERLIST} ]; then
-  # convert comma-separated string variables to arrays.
-  IFS=',' read -ra admin_array <<< "$PGB_ADMIN_USERS"
-  IFS=',' read -ra password_array <<< "$PGB_ADMIN_PASSWORDS"
-
-  # check every admin account has a corresponding password, and vice versa
-  if (( ${#admin_array[@]} != ${#password_array[@]} )); then
-      exit 1
-  fi
-
-  # Zip admin arrays together and write them to userlist.
-  for (( i=0; i < ${#admin_array[*]}; ++i )); do
-      echo "\"${admin_array[$i]}\" \"${password_array[$i]}\"" >> $USERLIST
-  done
-fi
+# Zip admin arrays together and write them to userlist.
+for (( i=0; i < ${#admin_array[*]}; ++i )); do
+    echo "\"${admin_array[$i]}\" \"${password_array[$i]}\"" >> $USERLIST
+done
 
 chmod 0600 $INI
 chmod 0600 $USERLIST
